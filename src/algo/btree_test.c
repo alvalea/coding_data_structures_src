13039a702ea4599e225eae8e3751cc1eeb7df4d9
alem <alem@gmv.com>
2023-05-08 17:23:16 +0200

    [BTree] Add generic unit test

--- src/algo/btree_test.c
+++ src/algo/btree_test.c
@@ -2,9 +2,25 @@
 
 #include <algo/container.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define STR 10
+typedef char string[STR];
+
+typedef struct Value {
+  int number;
+  string text;
+} Value;
 
 static
 int test_BTree_compare(void* key1, void* key2) {
+  Value* k1 = (Value*)key1;
+  Value* k2 = (Value*)key2;
+  return k1->number - k2->number;
+}
+
+static
+int test_BTree_compare_int(void* key1, void* key2) {
   int k1 = *(int*)key1;
   int k2 = *(int*)key2;
   return k1 - k2;
@@ -12,21 +28,25 @@ int test_BTree_compare(void* key1, void* key2) {
 
 static
 void test_BTree_print(void* value) {
-  int* v = (int*)value;
-  printf("%d.",*v);
+  Value* v = (Value*)value;
+  printf("%s.",v->text);
 }
 
 int test_BTree_insert() {
   int result = 0;
   for (int i=2; i<6; ++i) {
-    BTree* t = new_BTree(i, sizeof(int), sizeof(int), test_BTree_compare);
+    BTree* t = new_BTree(i, sizeof(Value), sizeof(Value), test_BTree_compare);
     {
       for (int j=1; j<=50; ++j) {
-        BTree_insert(t, &(int){j}, &(int){j});
+        Value v = { .number = j };
+        _itoa_s(j, v.text, STR, 10);
+        BTree_insert(t, &v, &v);
       }
       for (int j=1; j<=50; ++j) {
         if (j%3== 0) {
-          BTree_remove(t, &(int){j});
+          Value v = { .number = j };
+          _itoa_s(j, v.text, STR, 10);
+          BTree_remove(t, &v);
         }
       }
     }
@@ -37,7 +57,7 @@ int test_BTree_insert() {
 
 int test_BTree_find() {
   int result = 0;
-  BTree* t = new_BTree(2, sizeof(int), sizeof(int), test_BTree_compare);
+  BTree* t = new_BTree(2, sizeof(int), sizeof(int), test_BTree_compare_int);
   {
     for (int j=1; j<=50; ++j) {
       BTree_insert(t, &(int){j}, &(int){j});
